85ea49e3789b2d75cc89cd44397795aa6e2a6578
Dhruv Gupta <dhruv4@gmail.com>
2022-03-26 17:14:33 -0400

    update config

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@
  * and https://docs.magicmirror.builders/modules/configuration.html
  */
 let config = {
-	address: "localhost", 	// Address to listen on, can be:
+	address: "0.0.0.0", 	// Address to listen on, can be:
 							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
 							// - another specific IPv4/6 to listen on a specific interface
 							// - "0.0.0.0", "::" to listen on any interface
@@ -16,7 +16,7 @@ let config = {
 	port: 8080,
 	basePath: "/", 	// The URL path where MagicMirror is hosted. If you are using a Reverse proxy
 					// you must set the sub path here. basePath must end with a /
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], 	// Set [] to allow all IP addresses
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.255", "::ffff:192.168.1.18"], 	// Set [] to allow all IP addresses
 															// or add a specific IPv4 of 192.168.1.5 :
 															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
 															// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
@@ -57,13 +57,19 @@ let config = {
 			module: "calendar",
 			header: "Calendar",
 			position: "top_left",
-			maximumNumberOfDays: 3,
 			config: {
+				maximumNumberOfDays: 5,
+				// limitDays: 3,
 				calendars: [
 					{
 						symbol: "calendar-check",
 						url: "https://calendar.google.com/calendar/ical/dhruv4%40gmail.com/private-58a0f98c7317f74fd837a62d88ff2f3a/basic.ics",
 						color: "#002633"
+					},
+					{
+						symbol: "calendar-check",
+						url: "https://calendar.google.com/calendar/ical/dhruv%40axleapi.com/private-39e5c59792bf3c543e7807ea756a75c4/basic.ics",
+						color: "#FF0000"
 					}
 				]
 			}
@@ -92,7 +98,9 @@ let config = {
 				type: "forecast",
 				location: "Boston",
 				//locationID: "5128581", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-				apiKey: "8ea1189561130ddae7824922da356469"
+				apiKey: "8ea1189561130ddae7824922da356469",
+				// maxNumberOfDays: 10,
+				maxEntries: 10
 			}
 		},
 		{
@@ -124,7 +132,22 @@ let config = {
 		//    lite: false,
 		//    isSpeechVisualizationEnabled: true
 		//  }
-		//}
+		//},
+		{
+			module: 'MMM-Remote-Control',
+			// uncomment the following line to show the URL of the remote control on the mirror
+			// position: 'bottom_left',
+			// you can hide this module afterwards from the remote control itself
+			config: {
+				customCommand: {},  // Optional, See "Using Custom Commands" below
+				showModuleApiMenu: true, // Optional, Enable the Module Controls menu
+				secureEndpoints: true, // Optional, See API/README.md
+				// uncomment any of the lines below if you're gonna use it
+				// customMenu: "custom_menu.json", // Optional, See "Custom Menu Items" below
+				// apiKey: "", // Optional, See API/README.md for details
+				// classes: {} // Optional, See "Custom Classes" below
+			}
+		},
 	]
 };
 
